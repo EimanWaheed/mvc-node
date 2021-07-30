9d6d963f078066a5561bfca1177c0d6947b57b69
eiman.waheed@rolustech.net <eiman.waheed@RT-LT-471.rolustech.net>
2021-07-30 17:56:42 +0500

    RestController file added

--- core/controllers/restController.js
+++ core/controllers/restController.js
@@ -1,46 +1,70 @@
-/** Acquiring the modules. */
-const autoload = require(`${process.env.FILEPATH}/core/autoload.js`);
-const request = autoload('request');
-const modelFactory = autoload('modelFactory');
-const viewManager = autoload('viewManager');
-let requestInstance = request.getInstance();
 /** Class representing CRUD operations. */
 module.exports = class RestController {
 
-    /** Initialises the model object specified in the request. */
-    create() {
-        console.log("create() is called");
-        let modelObj = modelFactory.createModel(requestInstance.getController());
-        modelObj.create();
+    /**
+     * Initialises the model object specified in the request.
+     * @param {string} controllerName 
+     */
+    create(controllerName) {
+        const autoload = require(`${process.env.FILEPATH}/core/autoload.js`);
+        const modelFactory = autoload('modelFactory');
+        const viewManager = autoload('viewManager');
+        let modelObject = modelFactory.createModel(controllerName);
+        modelObject.create();
     }
-    update() {
+
+    /**
+     * Performs update operation.
+     * @param {string} controllerName 
+     */
+    update(controllerName) {
+        const autoload = require(`${process.env.FILEPATH}/core/autoload.js`);
+        const viewManager = autoload('viewManager');
         console.log("update() is called");
     }
-    list() {
+
+    /**
+     * Lists the items.
+     * @param {string} controllerName 
+     */
+    list(controllerName) {
+        const autoload = require(`${process.env.FILEPATH}/core/autoload.js`);
+        const viewManager = autoload('viewManager');
         console.log("list() is called");
     }
-    delete() {
+
+    /**
+     * Deletes specified record.
+     * @param {string} controllerName 
+     */
+    delete(controllerName) {
+        const autoload = require(`${process.env.FILEPATH}/core/autoload.js`);
+        const viewManager = autoload('viewManager');
         console.log("delete() is called");
     }
-    loadDefault() {
-        console.log("DefaultViewLoader() called");
-        /** View Manager is called. */
-        let controllerName=requestInstance.getController();
-        let actionName=requestInstance.getAction();
-        if(!controllerName){
-            controllerName='default';
-            actionName='defaultView';
-        }
-        viewManager.viewManager(controllerName, actionName);
+
+    /**
+     * Loads the default views using view manager.
+     * @param {string} controllerName 
+     */
+    loadDefault(controllerName) {
+        const autoload = require(`${process.env.FILEPATH}/core/autoload.js`);
+        const viewManager = autoload('viewManager');
+        return viewManager.loadView(controllerName, 'defaultView');
     }
-    /** Dynamically calls the action specified in the request. */
-    performAction() {
-        console.log("performAction() is called");
-        if (requestInstance.getAction()) {
-            this[requestInstance.getAction()]();
-        } else {
-            return this.loadDefault();
+
+    /**
+     * Dynamically calls the action specified in the request.
+     * @param {string} controllerName 
+     */
+    performAction(controllerName) {
+        const autoload = require(`${process.env.FILEPATH}/core/autoload.js`);
+        const requestInstance = autoload('request').getInstance();
+        let actionName = requestInstance.getAction();
+        if (!actionName) {
+            actionName = 'loadDefault';
         }
+        return this[actionName](controllerName);
     }
 
 }
