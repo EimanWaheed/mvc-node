206de415e940c62957f35d21f98f5d219322560a
eiman.waheed@rolustech.net <eiman.waheed@RT-LT-471.rolustech.net>
2021-08-02 18:36:59 +0500

    response file updated

--- core/response.js
+++ core/response.js
@@ -1,8 +1,18 @@
-
+/** Class representing response which is displayed on the screen. */
 module.exports=class Response{
+
+    /**
+     * Sets the html data.
+     * @param {string} htmlResponse 
+     */
     setContent(htmlResponse){
         this.htmlResponse=htmlResponse;
     }
+
+    /**
+     * Gets the html response.
+     * @return {string} The html response data.
+     */
     getContent(){
         return this.htmlResponse;
     }
