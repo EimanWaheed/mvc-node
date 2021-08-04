fa1fe082b822884c3e77c72ff80c506da4e0f184
eiman.waheed@rolustech.net <eiman.waheed@RT-LT-471.rolustech.net>
2021-08-04 17:51:08 +0500

    viewManager file updated

--- core/views/viewManager.js
+++ core/views/viewManager.js
@@ -1,6 +1,6 @@
 /** Acquiring autoloader. */
 const autoload = require(`${process.env.FILEPATH}/core/autoload.js`);
-        
+const mustache=require('mustache');
 /** Class representing View Manager which is responsible for loading view. */
 module.exports = class ViewManager {
 
@@ -14,8 +14,16 @@ module.exports = class ViewManager {
         const response = autoload('response');
         console.log("View Manager() is called");
         const htmlData = fs.readFileSync(`${process.env.FILEPATH}/app/views/${controllerName}/${actionName}.html`, 'utf-8');
-        response.setContent(htmlData);
+        const viewString = mustache.render(htmlData, this.data);
+        response.setContent(viewString);
         return response;
 
+    }
+    /**
+     * Sets the database fetched data in the class property.
+     * @param {string} data 
+     */
+    setData(data) {
+        this.data = data;
     }
 }
