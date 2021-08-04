d8a99b988ffba5a36b88239cc96b9f05669300ce
eiman.waheed@rolustech.net <eiman.waheed@RT-LT-471.rolustech.net>
2021-08-04 17:49:14 +0500

    driver file updated

--- core/models/database/driver/mysqli/driver.js
+++ core/models/database/driver/mysqli/driver.js
@@ -1,23 +1,49 @@
-const mysql = require('mysql');
+const mySql = require("sync-mysql");
+
+/** Class representing the database connection, for the purpose of setting the database connection.
+This class is responsible for running the query, thus the instance of this class is made singleton using Singleton Design Pattern. */
 module.exports = class Driver {
 
+    constructor() {
+        this.connection = new mySql({
+            host: "localhost",
+            user: "root",
+            password: "123",
+            database: "Master_MVC",
+        });
+    }
+
+    /**
+     * Get the singleton instance of the Driver class.
+     * @return {string} The Driver instance.
+     */
     static getInstance() {
         if (!Driver.instance) {
             Driver.instance = new Driver();
         }
         return Driver.instance;
     }
-    buildConnection() {
-        const connection = mysql.createConnection({
-            host: "localhost",
-            user: "root",
-            password: "123",
-            database: "Master_MVC",
-        });
-        return connection;
+
+    /**
+     * Get the connection string.
+     * @return {string} The connection string.
+     */
+    getConnection() {
+        return this.connection;
     }
+
+    /**
+     * Runs the query and returns the result.
+     * @param {string} queryString 
+     * @return {object} The fetched data object.
+     */
     runQuery(queryString) {
-        const connection = this.buildConnection();
-        connection.query(queryString);
+        try {
+            const connection = this.getConnection();
+            return connection.query(queryString);
+        }
+        catch (error) {
+            throw new Error("Database not connected.");
+        }
     }
 }
