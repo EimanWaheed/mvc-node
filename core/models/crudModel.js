d70affd65c9b506335d1d5a7cd1bbf8da94f7be7
eiman.waheed@rolustech.net <eiman.waheed@RT-LT-471.rolustech.net>
2021-08-04 17:48:11 +0500

    crudModel file updated

--- core/models/crudModel.js
+++ core/models/crudModel.js
@@ -1,39 +1,50 @@
 /** Acquiring autoloader. */
 const autoload = require(`${process.env.FILEPATH}/core/autoload.js`);
+const driverInstance = autoload('driver').getInstance();
 
 /** Class representing CRUD operations. */
 module.exports = class CRUDModel {
-    create() {
-        console.log("model create() is called");
-        const queryBuilder = autoload('queryBuilder');
-        const driver = autoload('driver');
-        this.entity.setEntity(autoload('request').getInstance().getParams());
-        console.log(this.entity);
-        const driverInstance = driver.getInstance();
-        let queryString = queryBuilder.create(this.entity);
+
+    /**
+     * Initialises the metadata of respective model, build the insert query and execute it.
+     * @param {object} params 
+     */
+    create(params) {
+        this.entity.setEntity(params);
+        let queryString = autoload('queryBuilder').create(this.entity);
         driverInstance.runQuery(queryString);
     }
-    update() {
-        console.log("update() is called");
-        const queryBuilder = autoload('queryBuilder');
-        const driver = autoload('driver');
-        this.entity.setEntity(autoload('request').getInstance().getParams());
-        console.log(this.entity);
-        const driverInstance = driver.getInstance();
-        let queryString = queryBuilder.update(this.entity);
+
+    /**
+     * Initialises the metadata of respective model, build the update query and execute it.
+     * @param {object} params 
+     */
+    update(params) {
+        this.entity.setEntity(params);
+        let queryString = autoload('queryBuilder').update(this.entity);
         driverInstance.runQuery(queryString);
     }
-    list() {
-        console.log("list() is called");
+
+    /**
+     * Initialises the metadata of respective model, build the select query and execute it.
+     * @param {object} params 
+     */
+    list(params) {
+        this.entity.setEntity(params);
+        let queryString = autoload('queryBuilder').list(this.entity);
+        let result = driverInstance.runQuery(queryString);
+        let templateKey = { 'data': [] };
+        templateKey.data = result;
+        return templateKey;
     }
-    delete() {
-        console.log(",odel delete() is called");
-        const queryBuilder = autoload('queryBuilder');
-        const driver = autoload('driver');
-        this.entity.setEntity(autoload('request').getInstance().getParams());
-        console.log(this.entity);
-        const driverInstance = driver.getInstance();
-        let queryString = queryBuilder.delete(this.entity);
+
+    /**
+     * Initialises the metadata of respective model, build the delete query and execute it.
+     * @param {object} params 
+     */
+    delete(params) {
+        this.entity.setEntity(params);
+        let queryString = autoload('queryBuilder').delete(this.entity);
         driverInstance.runQuery(queryString);
     }
 }
