667c4fe21a38f9b0cd27df23ed98a0edcbb12c6a
eiman.waheed@rolustech.net <eiman.waheed@RT-LT-471.rolustech.net>
2021-08-04 17:13:13 +0500

    course file updated

--- app/models/course.js
+++ app/models/course.js
@@ -2,9 +2,10 @@ const autoload = require(`${process.env.FILEPATH}/core/autoload.js`);
 const crudModel = autoload('crudModel');
 
 /** Class representing student model for creating an object. */
-module.exports = class courseModel extends crudModel {
+module.exports = class CourseModel extends crudModel {
     constructor() {
         super();
-        //console.log("course model is called")
+        const courseMetaData=autoload('courseMetaData');
+        this.entity=new courseMetaData();
     }
 }
