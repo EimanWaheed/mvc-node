7003e033edc062691706a94bf4f2a8af9291bff0
eiman.waheed@rolustech.net <eiman.waheed@RT-LT-471.rolustech.net>
2021-08-05 12:03:17 +0500

    crudModel file updated

--- core/models/crudModel.js
+++ core/models/crudModel.js
@@ -1,18 +1,35 @@
 /** Acquiring autoloader. */
 const autoload = require(`${process.env.FILEPATH}/core/autoload.js`);
-const driverInstance = autoload('driver').getInstance();
+const dbFactory = autoload('dbFactory');
 
 /** Class representing CRUD operations. */
 module.exports = class CRUDModel {
+
+    /**
+     * Returns the driver of the specified database. 
+     * @return {object} Driver object.
+     */
+    loadDriver() {
+        return dbFactory.loadDriver();
+    }
+
+    /**
+     * Returns the querybuilder of the specified database. 
+     * @return {object} QueryBuilder object.
+     */
+    loadQueryBuilder() {
+        return dbFactory.loadQueryBuilder();
+    }
 
     /**
      * Initialises the metadata of respective model, build the insert query and execute it.
      * @param {object} params 
      */
     create(params) {
+        console.log("crud create called");
         this.entity.setEntity(params);
-        let queryString = autoload('queryBuilder').create(this.entity);
-        driverInstance.runQuery(queryString);
+        let queryString = this.loadQueryBuilder().create(this.entity);
+        this.loadDriver().runQuery(queryString);
     }
 
     /**
@@ -21,8 +38,8 @@ module.exports = class CRUDModel {
      */
     update(params) {
         this.entity.setEntity(params);
-        let queryString = autoload('queryBuilder').update(this.entity);
-        driverInstance.runQuery(queryString);
+        let queryString = this.loadQueryBuilder().update(this.entity);
+        this.loadDriver().runQuery(queryString);
     }
 
     /**
@@ -31,8 +48,8 @@ module.exports = class CRUDModel {
      */
     list(params) {
         this.entity.setEntity(params);
-        let queryString = autoload('queryBuilder').list(this.entity);
-        let result = driverInstance.runQuery(queryString);
+        let queryString = this.loadQueryBuilder().list(this.entity);
+        let result = this.loadDriver().runQuery(queryString);
         let templateKey = { 'data': [] };
         templateKey.data = result;
         return templateKey;
@@ -44,7 +61,7 @@ module.exports = class CRUDModel {
      */
     delete(params) {
         this.entity.setEntity(params);
-        let queryString = autoload('queryBuilder').delete(this.entity);
-        driverInstance.runQuery(queryString);
+        let queryString = this.loadQueryBuilder().delete(this.entity);
+        this.loadDriver().runQuery(queryString);
     }
 }
