08261d61178ea5e4c5f812b197bd790676a56297
eiman.waheed@rolustech.net <eiman.waheed@RT-LT-471.rolustech.net>
2021-08-05 12:00:53 +0500

    studentMetaData file updated

--- app/models/metadata/studentMetaData.js
+++ app/models/metadata/studentMetaData.js
@@ -1,16 +1,19 @@
-
+/** Class representing Student Metadata which is responsible for acquiring all the properties of a student entity. */
 module.exports = class StudentMetaData {
     constructor() {
         console.log("student metadata is called");
         this.tableName = "student";
     }
 
+    /**
+     * Sets the params to the metadata of student.
+     * @param {object} params 
+     */
     setEntity(params) {
         console.log("setEntity() called");
         this.id = params['id'];
         this.name = params['name'];
         this.password = params['password'];
         this.email = params['email'];
-
     }
 }
