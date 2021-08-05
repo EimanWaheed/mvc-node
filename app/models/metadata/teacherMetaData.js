a15498784d48adab9818712d81b12511ed9993f3
eiman.waheed@rolustech.net <eiman.waheed@RT-LT-471.rolustech.net>
2021-08-05 12:01:42 +0500

    teacherMetaData file updated

--- app/models/metadata/teacherMetaData.js
+++ app/models/metadata/teacherMetaData.js
@@ -1,16 +1,19 @@
-
+/** Class representing Teacher Metadata which is responsible for acquiring all the properties of a teacher entity. */
 module.exports = class TeacherMetaData {
     constructor() {
         console.log("teacher metadata is called");
         this.tableName = "teacher";
     }
 
+    /**
+    * Sets the params to the metadata of teacher.
+    * @param {object} params 
+    */
     setEntity(params) {
         console.log("setEntity() called");
         this.id = params['id'];
         this.name = params['name'];
         this.password = params['password'];
         this.email = params['email'];
-
     }
 }
