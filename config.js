66bcb260f07e9b7f732421cf76759660d987c315
eiman.waheed@rolustech.net <eiman.waheed@RT-LT-471.rolustech.net>
2021-08-05 11:55:24 +0500

    Config file updated

--- config.js
+++ config.js
@@ -1,2 +1,5 @@
 /** Environment variable for getting parent working directory. */
 process.env.FILEPATH = process.env.PWD;
+
+/** Environment variable for specifying database to be used. */
+process.env.DBNAME = "mysqli";
