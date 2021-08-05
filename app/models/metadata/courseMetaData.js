cc2dd8a952f7c8d40e4bc4949fab48472158249c
eiman.waheed@rolustech.net <eiman.waheed@RT-LT-471.rolustech.net>
2021-08-05 12:00:14 +0500

    courseMetaData file updated

--- app/models/metadata/courseMetaData.js
+++ app/models/metadata/courseMetaData.js
@@ -1,10 +1,14 @@
-
+/** Class representing Course Metadata which is responsible for acquiring all the properties of a course entity. */
 module.exports = class CourseMetaData {
     constructor() {
         console.log("course metadata is called");
         this.tableName = "course";
     }
 
+    /**
+     * Sets the params to the metadata of course.
+     * @param {object} params 
+     */
     setEntity(params) {
         console.log("setEntity() called");
         this.id = params['id'];
