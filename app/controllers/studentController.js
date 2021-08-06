ca915c128e7960d646888523f94d030862b47434
eiman.waheed@rolustech.net <eiman.waheed@RT-LT-471.rolustech.net>
2021-08-06 11:39:05 +0500

    studentController file updated

--- app/controllers/studentController.js
+++ app/controllers/studentController.js
@@ -1,7 +1,12 @@
 const autoload = require(`${process.env.FILEPATH}/core/autoload.js`);
 const RestController = autoload('RestController');
 
-/** Class representing student controller for creating student object. */
+/**
+ * Class representing student controller which is responsible for identifying the controller
+ * to be set in the requestInstance and its instance will be used for performing the action 
+ * based on the action. Student controller is extending RestController which explicitly inherits
+ * all of the CRUD operations thus ensuring the reusability.
+ */
 module.exports = class studentController extends RestController {
     constructor() {
         super();
