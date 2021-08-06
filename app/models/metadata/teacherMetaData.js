9c0222e40eb1a0b2997c0464d44cbb90cb948b03
eiman.waheed@rolustech.net <eiman.waheed@RT-LT-471.rolustech.net>
2021-08-06 11:45:23 +0500

    teacherMetaData file updated

--- app/models/metadata/teacherMetaData.js
+++ app/models/metadata/teacherMetaData.js
@@ -1,4 +1,10 @@
-/** Class representing Teacher Metadata which is responsible for acquiring all the properties of a teacher entity. */
+/** 
+ * Class representing Teacher Metadata which is responsible for setting the metadata of
+ * Teacher havign properties of id, name, password and email. These properties must be in 
+ * accordance with the column names in database which is called Object Relation Mapping.
+ * The teacher metadata class is implementing the ORM (Object relation mapping) for the 
+ * synchronous mapping of data in the database. 
+ */
 module.exports = class TeacherMetaData {
     constructor() {
         this.tableName = "teacher";
@@ -6,7 +12,7 @@ module.exports = class TeacherMetaData {
 
     /**
     * Sets the params to the metadata of teacher.
-    * @param {object} params 
+    * @param {Object} params 
     */
     setEntity(params) {
         this.id = params['id'];
