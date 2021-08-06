300902f30c8237694b7364a837081bf824fb8f77
eiman.waheed@rolustech.net <eiman.waheed@RT-LT-471.rolustech.net>
2021-08-06 10:53:16 +0500

    restController file updated

--- core/controllers/restController.js
+++ core/controllers/restController.js
@@ -3,11 +3,22 @@ const autoload = require(`${process.env.FILEPATH}/core/autoload.js`);
 const modelFactory = autoload('modelFactory');
 const viewManager = autoload('viewManager');
 
-/** Class representing CRUD operations. */
+/** Class representing CRUD operations which will be responsible for generating 
+ * model object and returning the views for create, delete, list , update and default. 
+ * These CRUD operations are perfomed for the specific controller and thus 
+ * it generates the model object specified in each of these CRUD operations.
+ * This class is also responsible for returning all the views for the specified 
+ * controller and action.
+ */
 module.exports = class RestController {
 
     /**
-     * Initialises the model object specified in the request.
+     * Initialises the model object of the type specified in the request by 
+     * invoking the createModel method of the modelFactory, as the object is 
+     * created, it further invokes its respective CRUD operation which is
+     * create in this case and pass it the params achieved from request. The 
+     * next main task is to load the views for the specfied controller and 
+     * action and return it.
      * @param {string} controllerName 
      */
     create(controllerName, actionName) {
@@ -24,7 +35,12 @@ module.exports = class RestController {
     }
 
     /**
-     * Performs update operation.
+     * Initialises the model object of the type specified in the request by 
+     * invoking the createModel method of the modelFactory, as the object is 
+     * created, it further invokes its respective CRUD operation which is
+     * update in this case and pass it the params achieved from request. The 
+     * next main task is to load the views for the specfied controller and 
+     * action and return it.
      * @param {string} controllerName 
      */
     update(controllerName, actionName) {
@@ -41,7 +57,12 @@ module.exports = class RestController {
     }
 
     /**
-     * Lists the items.
+     * Initialises the model object of the type specified in the request by 
+     * invoking the createModel method of the modelFactory, as the object is 
+     * created, it further invokes its respective CRUD operation which is
+     * list in this case and pass it the params achieved from request. The 
+     * next main task is to load the views for the specfied controller and 
+     * action and return it.
      * @param {string} controllerName 
      */
     list(controllerName, actionName) {
@@ -58,7 +79,12 @@ module.exports = class RestController {
     }
 
     /**
-     * Deletes specified record.
+     * Initialises the model object of the type specified in the request by 
+     * invoking the createModel method of the modelFactory, as the object is 
+     * created, it further invokes its respective CRUD operation which is
+     * delete in this case and pass it the params achieved from request. The 
+     * next main task is to load the views for the specfied controller and 
+     * action and return it.
      * @param {string} controllerName 
      */
     delete(controllerName, actionName) {
@@ -75,7 +101,10 @@ module.exports = class RestController {
     }
 
     /**
-     * Loads the default views using view manager.
+     * Loads the view for the default case for each of the controller i.e.,
+     * student, teacher and course. It decides the viewing of default view on 
+     * the grounds of controller name and action which are received as params
+     * of the method.
      * @param {string} controllerName 
      */
     defaultView(controllerName, actionName) {
@@ -88,7 +117,9 @@ module.exports = class RestController {
     }
 
     /**
-     * Dynamically calls the action specified in the request.
+     * Dynamically calls the action specified in the params of the method. It
+     * decides which CRUD operation to call for the controller and thus invokes
+     * the CRUD operation by dynamically making the function call.
      * @param {string} controllerName 
      */
     performAction(controllerName, actionName) {
