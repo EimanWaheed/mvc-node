9f509f2d0ef77cba0c440ae5ee922bd81b8fc809
eiman.waheed@rolustech.net <eiman.waheed@RT-LT-471.rolustech.net>
2021-08-06 11:41:17 +0500

    studentModel file updated

--- app/models/student.js
+++ app/models/student.js
@@ -1,11 +1,16 @@
 const autoload = require(`${process.env.FILEPATH}/core/autoload.js`);
 const crudModel = autoload('crudModel');
 
-/** Class representing student model for creating an object. */
+/** 
+* Class representing student model which has the property of entity having all the metadata in
+* its instance which will be used for the running its own CRUD operations. Student Model is 
+* extending CrudModel which explicitly inherits all of the CRUD operations thus ensuring the 
+* reusability. The property of student model will make it easy to access its metadata.
+*/
 module.exports = class studentModel extends crudModel {
     constructor() {
         super();
-        const studentMetaData=autoload('studentMetaData');
-        this.entity=new studentMetaData();
+        const studentMetaData = autoload('studentMetaData');
+        this.entity = new studentMetaData();
     }
 }
