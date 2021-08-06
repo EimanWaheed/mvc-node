616325ad082b6fec86a17898c0de1c08a9e6793c
eiman.waheed@rolustech.net <eiman.waheed@RT-LT-471.rolustech.net>
2021-08-06 10:52:27 +0500

    controllerFactory file updated

--- core/controllers/controllerFactory.js
+++ core/controllers/controllerFactory.js
@@ -1,4 +1,5 @@
-/** Class representing the factory for building specified controller. It is responsible for building the
+/** 
+ * Class representing the factory for building specified controller. It is responsible for building the
  * specified controller object. Factory Design Pattern is used in this class for creating desired objects
  * polymorphically. The object made is defined already and thus the required instance is returned.
  */
@@ -7,7 +8,9 @@ module.exports = class controllerFactory {
     /**
     * Creates controller of type specified by getting the controllerName specified 
     * as a parameter in the function definition. It creates the specific controller object 
-    * using the environment variable from config.js and returns the controller object created.
+    * using the environment variable from config.js and returns the controller object created. Error 
+    * handling is also done in the case if the controller is not created due to some reasons,
+    * just throw the error specifying the problem message.
     * @param {string} controllerName 
     */
     createController(controllerName) {
