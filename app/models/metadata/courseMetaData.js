18ad2e8526a0a8bc6bcd393e6133674a96a83ec8
eiman.waheed@rolustech.net <eiman.waheed@RT-LT-471.rolustech.net>
2021-08-06 11:42:03 +0500

    courseMetaData file updated

--- app/models/metadata/courseMetaData.js
+++ app/models/metadata/courseMetaData.js
@@ -1,4 +1,10 @@
-/** Class representing Course Metadata which is responsible for acquiring all the properties of a course entity. */
+/** 
+ * Class representing Course Metadata which is responsible for setting the metadata of
+ * Course havign properties of id, name and courseCode. These properties must be in 
+ * accordance with the column names in database which is called Object Relation Mapping.
+ * The course metadata class is implementing the ORM (Object relation mapping) for the 
+ * synchronous mapping of data in the database. 
+ */
 module.exports = class CourseMetaData {
     constructor() {
         this.tableName = "course";
@@ -6,7 +12,7 @@ module.exports = class CourseMetaData {
 
     /**
      * Sets the params to the metadata of course.
-     * @param {object} params 
+     * @param {Object} params 
      */
     setEntity(params) {
         this.id = params['id'];
