0b92c1368276575f2f678b1683b33f544a7ed367
eiman.waheed@rolustech.net <eiman.waheed@RT-LT-471.rolustech.net>
2021-08-06 11:37:49 +0500

    courseController file updated

--- app/controllers/courseController.js
+++ app/controllers/courseController.js
@@ -1,7 +1,12 @@
 const autoload = require(`${process.env.FILEPATH}/core/autoload.js`);
 const RestController = autoload('RestController');
 
-/** Class representing course controller for creating course object. */
+/**
+ * Class representing course controller which is responsible for identifying the controller
+ * to be set in the requestInstance and its instance will be used for performing the action 
+ * based on the action. Course controller is extending RestController which explicitly inherits
+ * all of the CRUD operations thus ensuring the reusability.
+ */
 module.exports = class courseController extends RestController {
     constructor() {
         super();
