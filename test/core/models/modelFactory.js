602fc943a75f10286d3530127e5a5470a81671b3
eiman.waheed@rolustech.net <eiman.waheed@RT-LT-471.rolustech.net>
2021-08-11 11:10:35 +0500

    modelFactory file updated

--- test/core/models/modelFactory.js
+++ test/core/models/modelFactory.js
@@ -1,12 +1,12 @@
-require('/home/eiman.waheed/Desktop/mvc-node/config.js');
 const autoload = require(`${process.env.FILEPATH}/core/autoload.js`);
-
 QUnit.module("Model Factory", function (assert) {
     const actualObject = autoload('modelFactory').createModel('student');
     const expectedObject = new (require(`${process.env.FILEPATH}/app/models/student.js`))
+    /** Model Factory success case */
     QUnit.test('correct controller', function (assert) {
         assert.deepEqual(actualObject, expectedObject);
     });
+     /** Model Factory exception case */
     QUnit.test('Exception controller', function (assert) {
         assert.throws(function () {
             autoload('controllerFactory').createController('books');
