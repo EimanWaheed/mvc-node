0bd06631a90482668e3356a336ffc63aada70b40
Eiman Waheed <eimanwaheed042@gmail.com>
2021-11-21 16:30:36 +0500

    Git branching commit added

---
+++ test.js
@@ -0,0 +1,10 @@
+/**
+ * Test Function for testing GIT branching.
+ */
+function testFunction(){
+    console.log("Hello World");
+    console.log("another world again");
+}
+
+/** Calling function. */
+testFunction();
